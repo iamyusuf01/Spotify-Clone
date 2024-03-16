fe38c8fa4e2fa6fea85efe430b8aae6bc380b755
Md Yusuf Ansari <100552609+iamyusuf01@users.noreply.github.com>
2024-03-17 00:10:40 +0530

    Day5

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("hi");
 
+// data fatching through a folder songs 
 async function getSongs() 
 {
     let a = await fetch("http://127.0.0.1:5500/songs/")
@@ -13,7 +14,7 @@ async function getSongs()
     {
        const element = as[index];
        if (element.href.endsWith(".mp3")){
-        songs.push(element.href)
+        songs.push(element.href.split("/songs/")[1]);
        }
     }
     return songs
@@ -23,16 +24,31 @@ async function main() {
     let songs = await getSongs()
     console.log(songs)
 
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul") [0]
+        for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> 
+                            <img class="invert" src="music.svg" alt="">
+                            <div class="info">
+                                <div>${song.replaceAll("%20"," ")}</div>
+                                <div>DevOps</div>
+                            </div>
+                            <div class="playnow">
+                                <span>PlayNow</span>
+                                <img class="invert" src="play.svg" alt="">
+                            </div>
+                            
+                        </li>`;    
+        }
+
     //play first songs
 
     var audio = new Audio(songs[0]);
-    audio.play(); 
+    audio.play();
 
     audio.addEventListener("loadeddata", () => {
         console.log(audio.duration, audio.currentSrc, audio.currentTime)
         //the duration variable now hold the duration (in seconds)of the audio clip
     });
-
 
 }
 
